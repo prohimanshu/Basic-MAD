c177b187f426a5b12fbd9fe75b690b783aa298e8
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 15:10:54 +0530

    Date and time picker

---
+++ 5 WorkingWithDateTimePickers/WorkingWithDateTimePickers/WorkingWithDateTimePickers/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkingWithDateTimePickers
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Buttn_Clicked(object sender, EventArgs e)
+        {
+            var date = dp.Date;
+            var time = tp.Time;
+            details.Text = string.Format("Date:{0} \n Time:{1}", date, time);
+
+        }
+
+    }
+}
