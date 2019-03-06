3297851d8a4bb33896bac00906ec852922aa0fcf
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 17:08:56 +0530

    image basic

---
+++ 17 ImageBasic/ImageBasic/ImageBasic/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageBasic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Myimage.Source = ImageSource.FromResource("ImageBasic.image.Capture.PNG");
+        }
+    }
+}
