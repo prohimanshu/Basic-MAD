9d965d2b54199073ec161f5d02e6c95bef3c407b
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:10:58 +0530

    item template basic

---
+++ 9 ItemTemplate/ItemTemplate/ItemTemplate/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemTemplate
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ 9 ItemTemplate/ItemTemplate/ItemTemplate/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ItemTemplate
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Data> people = new List<Data>()
+        {
+            new Data{Name="Himanshu",Address="Dehradun"},
+            new Data{Name="Mohit",Address="Pori"},
+            new Data{Name="Hari",Address="USA"}
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            mylist.ItemsSource = people;
+        }
+    }
+}
