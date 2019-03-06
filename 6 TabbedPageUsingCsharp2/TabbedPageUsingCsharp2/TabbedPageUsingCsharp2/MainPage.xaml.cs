3544905b54c6ab82d4e922b53685649c27ecc499
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 15:39:47 +0530

    again using C# tabbed page

---
+++ 6 TabbedPageUsingCsharp2/TabbedPageUsingCsharp2/TabbedPageUsingCsharp2/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedPageUsingCsharp2
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Children.Add(new Page1() { Title = "Page 1", Icon = "abc.png" });
+            Children.Add(new Page2() { Title = "Page 2", Icon = "def.png" });
+        }
+    }
+}
