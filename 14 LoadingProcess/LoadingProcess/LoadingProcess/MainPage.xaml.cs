c1d612bb2a1b048c00488b8275efc2a9320b1e22
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 17:20:36 +0530

    process bar

---
+++ 14 LoadingProcess/LoadingProcess/LoadingProcess/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoadingProcess
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Getdata(object sender, EventArgs e)
+        {
+            CheckConnectivity();
+            var url = "http://" + urlEntry.Text;
+            Browser.Source = url;
+        }
+
+        private void CheckConnectivity()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                //DisplayAlert("Message", "Internet Connected", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Message", "No Internet Connection", "Ok");
+            }
+        }
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            Progress.IsVisible = false;
+            Progress.IsRunning = false;
+        }
+
+        private void Browser_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            Progress.IsVisible = true;
+            Progress.IsRunning = true;
+        }
+    }
+}
