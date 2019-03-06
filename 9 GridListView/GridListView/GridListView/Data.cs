a80f5c2f5e6f579e10242ecb338e146dbb1f88c4
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:05:55 +0530

    grid list view

---
+++ 9 GridListView/GridListView/GridListView/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridListView
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ 9 GridListView/GridListView/GridListView/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Data> people = new List<Data>()
+        {
+            new Data{Name="Himanshu", Address="Dehradun",ID=1},
+            new Data{Name="Rahul",Address="Delhi",ID=2},
+            new Data{Name="Mohit",Address="Kalsi",ID=3}
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            Mylist.ItemsSource = people;
+        }
+    }
+}
