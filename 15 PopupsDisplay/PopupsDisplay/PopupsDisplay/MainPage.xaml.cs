b04095f1132c8e87ac1d1119b7e7a07bb5203300
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 17:07:09 +0530

    message type

---
+++ 15 PopupsDisplay/PopupsDisplay/PopupsDisplay/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupsDisplay
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickHear(object sender, EventArgs e)
+        {
+            var response =await DisplayAlert("Message", "Do you want to change Color(Green)", "Ok", "No");
+            if (response)
+            {
+                this.BackgroundColor = Color.Green;
+            }
+            else
+            {
+                this.BackgroundColor = Color.White;
+            }
+        }
+    }
+}
