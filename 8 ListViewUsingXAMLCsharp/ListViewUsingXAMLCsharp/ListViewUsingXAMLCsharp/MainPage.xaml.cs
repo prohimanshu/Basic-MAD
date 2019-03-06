52f61ccd98eef1e2ad98846d15233183a81e0628
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:01:04 +0530

    list view testing using xaml

---
+++ 8 ListViewUsingXAMLCsharp/ListViewUsingXAMLCsharp/ListViewUsingXAMLCsharp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewUsingXAMLCsharp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            mylist.ItemsSource = new List<string>
+            {
+                "Apple","Orange","Banana","Gavava"
+            };
+        }
+    }
+}
