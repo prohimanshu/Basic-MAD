26ca07027d3021cd81f452997a6c95f749acc0e2
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 17:12:17 +0530

    action sheet basic

---
+++ 20 ActionSheetbasic/ActionSheetbasic/ActionSheetbasic/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ActionSheetbasic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickedAction(object sender, EventArgs e)
+        {
+            var action=await DisplayActionSheet("Select Country", "Cancel","", "India", "USA", "UK", "Australia");
+            if(action== "India")
+            {
+                ActionDetils.Text = "Select Country" + action;
+            }
+            if (action == "USA")
+            {
+                ActionDetils.Text = "Select Country" + action;
+            }
+            if (action == "UK")
+            {
+                ActionDetils.Text = "Select Country" + action;
+            }
+            if (action == "Australia")
+            {
+                ActionDetils.Text = "Select Country" + action;
+            }
+        }
+    }
+}
