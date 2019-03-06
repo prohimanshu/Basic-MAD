cc24957845b6a9c6fea0cb7fcf00c4235c2957af
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 14:49:07 +0530

    final UI connected to C#

---
+++ 3 LoginPage UI using C#/LoginPage/LoginPage/LoginPage/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            this.BackgroundColor = Color.FromHex("#2ccbff");
+            var userLabel = new Label { Text = "Username", TextColor = Color.Black };
+            var userentry = new Entry { Placeholder = "Username" };
+            var passLabel = new Label { Text = "Password", TextColor = Color.Black };
+            var passentry = new Entry { Placeholder = "Password", IsPassword = true };
+            var loginbutton = new Button { Text = "Login", BackgroundColor = Color.White, TextColor = Color.FromHex("2ccbff") };
+            var regLabel = new Label { Text = "Not a Member? Sign up now", TextColor = Color.Gray, HorizontalOptions = LayoutOptions.Center };
+            loginbutton.Clicked += LoginEvent;
+            this.Content = new StackLayout
+            {
+                Padding = new Thickness(20),
+                Margin = new Thickness(0, 80, 0, 0),
+                Children =
+                {
+                    userLabel,userentry,passLabel,passentry,loginbutton
+                }
+            };
+        }
+
+        private void LoginEvent(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull", "Ok");
+        }
+    }
+}
+
