5ec6e05b579ba93953b27e1d59287a5e027a43d4
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 15:01:29 +0530

    drop down test

---
+++ 5 PickersDropDownMenu/PickersDropDownMenu/PickersDropDownMenu/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickersDropDownMenu
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Buttn_Clicked(object sender, EventArgs e)
+        {
+            var date = dp.Date;
+            var time = tp.Time;
+            var pickerdata = MyPicker.SelectedItem.ToString();
+            details.Text = string.Format("Date:{0} \n Time:{1} \n Location:{2}", date, time, pickerdata);
+
+        }
+    }
+}
