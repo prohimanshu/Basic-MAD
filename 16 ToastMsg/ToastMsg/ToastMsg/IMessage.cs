0dde6ca2e24814de65e83334f4df2ed6d6170784
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 17:07:55 +0530

    tost msg

---
+++ 16 ToastMsg/ToastMsg/ToastMsg/IMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToastMsg
+{
+    public interface IMessage
+    {
+        void Longtime(string message);
+        void Shorttime(string message);
+    }
+}
---
+++ 16 ToastMsg/ToastMsg/ToastMsg/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToastMsg
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var message = "This is Long Time Message";
+            DependencyService.Get<IMessage>().Longtime(message);
+            //DependencyService.Get<IMessage>().Shorttime(message);
+        }
+    }
+}
