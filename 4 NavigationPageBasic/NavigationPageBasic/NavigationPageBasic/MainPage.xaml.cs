6f26e3cba051a41b488a58432d39e8a882a5da8d
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 14:57:35 +0530

    mult screen again

---
+++ 4 NavigationPageBasic/NavigationPageBasic/NavigationPageBasic/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationPageBasic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void LoginEvent(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull", "Ok");
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
