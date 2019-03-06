3340e8db3fecba45f52d1efc143924cc06445574
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 14:45:06 +0530

    login page

---
+++ 3 LoginPage/LoginPage/LoginPage/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginBtn(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull", "Ok");
+                        //hedder //for show string //button text
+        }
+    }
+}
