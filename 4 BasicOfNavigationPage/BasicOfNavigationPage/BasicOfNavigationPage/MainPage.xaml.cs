e110ff0beeb90b790996aac498f7926840b66b8a
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 14:53:18 +0530

    multipe screen

---
+++ 4 BasicOfNavigationPage/BasicOfNavigationPage/BasicOfNavigationPage/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BasicOfNavigationPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void LoginEvent(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull", "Ok");
+            Navigation.PushAsync(new HomePage(usernameentry.Text));
+        }
+    }
+}
