915ea5631c2978f6392861c813a8469824999d2e
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:17:21 +0530

    item template test

---
+++ 9 ItemTemplate2/ItemTemplate2/ItemTemplate2/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemTemplate2
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Images { get; set; }
+    }
+}
---
+++ 9 ItemTemplate2/ItemTemplate2/ItemTemplate2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ItemTemplate2
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Data> people = new List<Data>()
+        {
+            new Data{Name="Himanshu",Address="HN03,Utail Dehradun,248158",Images="abcd.jpg"},
+            new Data{Name="Rahul",Address="Pori Uttarakhand",Images="abcd.jpg"},
+            new Data{Name="Mohit",Address="Vikasnagar Dehradun",Images="abcd.jpg"}
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            Mylist.ItemsSource = people;
+        }
+    }
+}
