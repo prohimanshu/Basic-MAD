d923ecf1e8e3934144785eaed54459fd3c7bb57b
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 15:54:34 +0530

    list view using c#

---
+++ 8 ListViewUsingCsharp/ListViewUsingCsharp/ListViewUsingCsharp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewUsingCsharp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var mylist = new ListView();
+            mylist.ItemsSource = new List<string>
+            {
+                "Apple","Banana","Gavava","Orange","Mango"
+            };
+            Content = mylist;
+        }
+    }
+}
