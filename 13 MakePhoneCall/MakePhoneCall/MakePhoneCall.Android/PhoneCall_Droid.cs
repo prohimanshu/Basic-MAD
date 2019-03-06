81d10884120ba0daae854f3ed9951ac2267cfdc6
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:56:56 +0530

    phone call testing

---
+++ 13 MakePhoneCall/MakePhoneCall/MakePhoneCall.Android/PhoneCall_Droid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MakePhoneCall.Droid;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(PhoneCall_Droid))]
+
+namespace MakePhoneCall.Droid
+{
+    public class PhoneCall_Droid : IPhoneCall
+    {
+        public void makecall(string phonenumber)
+        {
+            try
+            {
+
+           
+            var URI = Android.Net.Uri.Parse(string.Format("tel:{0}", phonenumber));
+            var intent = new Intent(Intent.ActionCall, URI);
+            Xamarin.Forms.Forms.Context.StartActivity(intent);
+            }
+            catch(Exception ex)
+            {
+                string msg = ex.Message;
+            }
+        }
+    }
+}
---
+++ 13 MakePhoneCall/MakePhoneCall/MakePhoneCall/IPhoneCall.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakePhoneCall
+{
+    public interface IPhoneCall
+    {
+        void makecall(string phonenumber);
+    }
+}
