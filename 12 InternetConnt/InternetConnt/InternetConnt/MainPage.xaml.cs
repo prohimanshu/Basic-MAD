8657e6f81f5ba6acb9395c5f549a91322126addc
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 16:51:32 +0530

    internet connection testing

---
+++ 12 InternetConnt/InternetConnt/InternetConnt/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InternetConnt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Getdata(object sender, EventArgs e)
+        {
+            CheckConnectivity();
+            var url = "http://" + urlEntry.Text;
+            Browser.Source = url;
+        }
+
+        private void CheckConnectivity()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                DisplayAlert("Message", "Internet Connected", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Message", "No Internet Connection", "Ok");
+            }
+        }
+    
+    }
+}
