0d1e1d150c6a83ff0d819a520936e8496d731989
Himanshu Chauhan <coderhimanshu@outlook.com>
2019-03-06 15:22:04 +0530

    Tabbed Page test again

---
+++ 6 TabbedPageBasic1/TabbedPageBasic1/TabbedPageBasic1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace TabbedPageBasic1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new MainPage(),new Page1(),new Page2()
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
